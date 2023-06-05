986811bd6efe207874c89b36c148dbc584671e11
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-05 11:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,56 +1,69 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const video = document.querySelector('.flex');
-  const progressBar = document.querySelector('.progress__filled');
-  const playButton = document.querySelector('.player__button');
-  const volumeSlider = document.querySelector('input[name="volume"]');
-  const playbackSpeedSlider = document.querySelector('input[name="playbackRate"]');
-  const skipButtons = document.querySelectorAll('.skip__button');
+// Get the necessary DOM elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggleButton = player.querySelector('.toggle');
+const volumeSlider = player.querySelector('input[name="volume"]');
+const playbackRateSlider = player.querySelector('input[name="playbackRate"]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 
-  // Play or pause the video
-  function togglePlay() {
-    if (video.paused) {
-      video.play();
-    } else {
-      video.pause();
-    }
+// Function to toggle play/pause
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
   }
+}
 
-  // Update the play button icon
-  function updatePlayButton() {
-    playButton.textContent = video.paused ? '►' : '❚ ❚';
-  }
+// Function to update the play/pause button text
+function updateButton() {
+  const icon = video.paused ? '►' : '❚ ❚';
+  toggleButton.textContent = icon;
+}
 
-  // Update the progress bar
-  function updateProgress() {
-    const progressPercentage = (video.currentTime / video.duration) * 100;
-    progressBar.style.flexBasis = `${progressPercentage}%`;
-  }
+// Function to handle volume change
+function handleVolumeChange() {
+  video.volume = volumeSlider.value;
+}
 
-  // Skip forward or backward in the video
-  function skip() {
-    const skipTime = parseFloat(this.dataset.skip);
-    video.currentTime += skipTime;
-  }
+// Function to handle playback rate change
+function handlePlaybackRateChange() {
+  video.playbackRate = playbackRateSlider.value;
+}
 
-  // Set the volume of the video
-  function setVolume() {
-    video.volume = this.value;
-  }
+// Function to skip the video by a specified duration
+function skip() {
+  const skipAmount = parseFloat(this.dataset.skip);
+  video.currentTime += skipAmount;
+}
 
-  // Set the playback speed of the video
-  function setPlaybackSpeed() {
-    video.playbackRate = this.value;
-  }
+// Function to update the progress bar
+function updateProgress() {
+  const progressPercentage = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${progressPercentage}%`;
+}
 
-  // Event listeners
-  video.addEventListener('click', togglePlay);
-  video.addEventListener('play', updatePlayButton);
-  video.addEventListener('pause', updatePlayButton);
-  video.addEventListener('timeupdate', updateProgress);
+// Event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', updateProgress);
 
-  playButton.addEventListener('click', togglePlay);
+toggleButton.addEventListener('click', togglePlay);
 
-  volumeSlider.addEventListener('input', setVolume);
-  playbackSpeedSlider.addEventListener('input', setPlaybackSpeed);
+volumeSlider.addEventListener('input', handleVolumeChange);
+playbackRateSlider.addEventListener('input', handlePlaybackRateChange);
 
-  skipButtons
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+progress.addEventListener('click', (e) => {
+  const progressWidth = progress.offsetWidth;
+  const clickX = e.offsetX;
+  const seekTime = (clickX / progressWidth) * video.duration;
+  video.currentTime = seekTime;
+});
+
+// Initial setup
+updateButton();
